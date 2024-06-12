d84d8fb49c861c0f5de89c2fd0e283e9f5acad84
Lucas Holman <Holman.Lucas.J@gmail.com>
2024-06-12 09:08:28 -0700

    Finished initial iterations

--- app/src/main/java/com/example/ferthcounters/MainActivity.java
+++ app/src/main/java/com/example/ferthcounters/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.ferthcounters;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
         WebView webView=findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
+
         webView.loadUrl("file:///android_asset/index.html");
     }
 }
